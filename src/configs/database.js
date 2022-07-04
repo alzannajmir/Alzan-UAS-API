c838fad4733aca46e63ab25b8377d2bb3e5525c2
Rizki Awal R <89265141+Awal5@users.noreply.github.com>
2022-07-04 13:41:58 +0700

    Update database.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     multipleStatements: true,
     host: 'db4free.net',
-    user: 'hwangyeji',
-    password: 'Einshearth1111',
-    database: 'pkl_ay'
+    user: 'rizkiawal2',
+    password: 'darahmuda2',
+    database: 'handphone_data'
 };
