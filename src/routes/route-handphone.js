f53be98722cfe6b9916f5012129445c7c90a4733
alzannajmir <104343120+alzannajmir@users.noreply.github.com>
2022-07-04 20:21:16 +0700

    Update route-handphone.js

--- src/routes/route-handphone.js
+++ src/routes/route-handphone.js
@@ -4,18 +4,18 @@ const {
 } = require('../controllers');
 
 // GET localhost:8080/handphone =› Ambil data semua handphone
-router.get('/alzan_najmi', handphone.getDataHandphone);
+router.get('/alzan_najmi', alzan_najmi.getDataHandphone);
 
 // GET localhost:8080/handphone/2=›Ambildatasemuahandphoneberda
-router.get('/alzan_najmi/:id_HP', handphone.getDataHandphoneByID);
+router.get('/alzan_najmi/:id_HP', alzan_najmi.getDataHandphoneByID);
 
 // POST localhost:8080/handphone/add=›Tambahdatahandphonekedatat
-router.post('/alzan_najmi/add', handphone.addDataHandphone);
+router.post('/alzan_najmi/add', alzan_najmi.addDataHandphone);
 
 // POST localhost:8080/handphone/2=›Edit data handphone
-router.post('/alzan_najmi/edit', handphone.editDataHandphone);
+router.post('/alzan_najmi/edit', alzan_najmi.editDataHandphone);
 
 // POST localhost:8080/handphone/delete=>Delete data handphone
-router.post('/alzan_najmi/delete/', handphone.deleteDataHandphone);
+router.post('/alzan_najmi/delete/', alzan_najmi.deleteDataHandphone);
 
 module.exports = router;
