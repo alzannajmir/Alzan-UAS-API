9236aa7896488a56246cb30b680e1d18fec281d4
alzannajmir <104343120+alzannajmir@users.noreply.github.com>
2022-07-04 20:08:45 +0700

    Update database.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     multipleStatements: true,
     host: 'db4free.net',
-    user: 'rizkiawal2',
-    password: 'darahmuda2',
-    database: 'handphone_data'
+    user: 'alzannajmi',
+    password: 'duaribu11desember',
+    database: 'alzan_najmi'
 };
